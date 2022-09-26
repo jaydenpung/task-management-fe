c8e288088d1b3e8c242d831fdf4077c80d89855c
jaydenpung <46307126+jaydenpung@users.noreply.github.com>
2022-09-27 04:12:46 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/task',
+        permanent: true,
+      },
+    ]
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
-
-module.exports = nextConfig
