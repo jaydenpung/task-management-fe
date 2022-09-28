b437a440b378ce4f2c68fa90f07d8a7b4efe7550
jaydenpung <46307126+jaydenpung@users.noreply.github.com>
2022-09-28 18:07:28 +0800

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://task-management-api-777.herokuapp.com/"
+export const BASE_URL = "https://task-management-api-666.herokuapp.com/"
+//export const BASE_URL = "http://localhost:3001/"
